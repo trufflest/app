8a3aa687ed8004daeff7b9022c6fe880ddfd3d1e
vaux <zero.griffin@gmail.com>
2022-02-10 19:04:08 +0100

    Update multitouch

--- Scenes/Scene.swift
+++ Scenes/Scene.swift
@@ -16,13 +16,14 @@ class Scene: SKScene {
         addChild(cornelius)
         addChild(joystick)
         addChild(jump)
-
+    
         cornelius.position = .init(x: 100, y: 40)
         joystick.position = .init(x: 70 + 95, y: 195)
     }
     
     final override func didMove(to: SKView) {
         jump.position = .init(x: to.bounds.width - 70 - 60, y: 195)
+        to.isMultipleTouchEnabled = true
     }
     
     final override func update(_ currentTime: TimeInterval) {
@@ -52,40 +53,66 @@ class Scene: SKScene {
     }
     
     private func update(x: Int, y: Int) {
-        let base = ground.tileDefinition(atColumn: x, row: y)
-        
-        if base == nil {
-            cornelius.run(.moveBy(x: 0, y: -tile, duration: cooldown))
+        if ground.tileDefinition(atColumn: x, row: y) == nil {
+            var delta = -tile
+            for index in (0 ... 2) {
+                guard y - index >= 0, ground.tileDefinition(atColumn: x, row: y - index) == nil else {
+                    break
+                }
+                delta -= tile
+            }
             
-            if cornelius.position.y <= 20 {
+            cornelius.run(.moveBy(x: 0, y: delta, duration: cooldown))
+            
+            if cornelius.position.y - delta < 20 {
                 self.isPaused = true
             }
         }
     }
     
     private func updateActions(x: Int, y: Int) {
+        var jumped = false
+        let grounded = ground.tileDefinition(atColumn: x, row: y) != nil
+        if grounded && jump.state {
+            
+            var delta = 0.0
+            for x in (2 ... 5) {
+                guard ground.tileDefinition(atColumn: x, row: y + x) == nil else {
+                    break
+                }
+                delta += tile
+            }
+            cornelius.run(.moveBy(x: 0, y: delta, duration: cooldown))
+            cornelius.state = .jump
+            jumped = true
+        }
+        
         switch joystick.state {
         case .left:
             switch cornelius.state {
             case .walk1:
                 cornelius.state = .walk2
-            default:
+            case .walk2, .none:
                 cornelius.state = .walk1
+            default:
+                break
             }
             
             if cornelius.facing == .right {
                 cornelius.facing = .left
             }
             
-            if x > 1 {
+            if x > 1 && ground.tileDefinition(atColumn: x - 1, row: y + 1) == nil {
                 cornelius.run(.moveBy(x: -tile, y: 0, duration: cooldown))
             }
         case .right:
             switch cornelius.state {
             case .walk1:
                 cornelius.state = .walk2
-            default:
+            case .walk2, .none:
                 cornelius.state = .walk1
+            default:
+                break
             }
             
             if cornelius.facing == .left {
@@ -94,7 +121,7 @@ class Scene: SKScene {
             
             cornelius.run(.moveBy(x: tile, y: 0, duration: cooldown))
         case .none:
-            if cornelius.state != .none {
+            if cornelius.state != .none && grounded && !jumped {
                 cornelius.state = .none
             }
         }
